c883f7ae9f3ccdf0758c52fe71263f0039c97444
Jihad <jihad513511@gmail.com>
2022-11-20 20:33:27 +0700

    Initial commit

---
+++ app/src/main/java/com/f55121063/mylistview/moh_jihad_khalid.java
@@ -0,0 +1,14 @@
+package com.f55121063.mylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class moh_jihad_khalid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moh_jihad_khalid);
+    }
+}
---
+++ app/src/main/java/com/f55121063/mylistview/sahrul.java
@@ -0,0 +1,14 @@
+package com.f55121063.mylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sahrul extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sahrul);
+    }
+}
